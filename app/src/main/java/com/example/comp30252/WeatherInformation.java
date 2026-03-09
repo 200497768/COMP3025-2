5fdf500cbb48cad200e0d25a829bc855f6c9c0a5
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-09 12:35:35 -0400

    I added the WeatherInformation class.

---
+++ app/src/main/java/com/example/comp30252/WeatherInformation.java
@@ -0,0 +1,10 @@
+package com.example.comp30252;
+
+/**
+ * This class is weather information that has been retrieved.
+ *
+ * @author Hao Tian
+ * @author Yatri Devangbhai Padhiyar
+ */
+public class WeatherInformation {
+}
