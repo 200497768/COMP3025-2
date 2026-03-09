bc46aeca48e0945e0b8bde1622937c9dbcc915b9
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-09 12:01:58 -0400

    I created CityFragment to allow a city to be chosen.

---
+++ app/src/main/java/com/example/comp30252/CityFragment.java
@@ -0,0 +1,28 @@
+package com.example.comp30252;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * This fragment allows a city to be chosen.
+ *
+ * @author Hao Tian
+ * @author Yatri Devangbhai Padhiyar
+ */
+public class CityFragment extends Fragment {
+
+    public CityFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_city, container, false);
+    }
+}
