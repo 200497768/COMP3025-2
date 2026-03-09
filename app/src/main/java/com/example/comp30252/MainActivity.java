82202c4b966430cb5ffd50f1d98511bd61729611
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-09 10:11:01 -0400

    I removed text for the onCreate method that I didn't write.

--- app/src/main/java/com/example/comp30252/MainActivity.java
+++ app/src/main/java/com/example/comp30252/MainActivity.java
@@ -22,11 +22,6 @@ public class MainActivity extends AppCompatActivity {
     /**
      * The onCreate method adds views to show retrieved information.
      * This method might happen repeatedly.
-     *
-     * @param savedInstanceState If the activity is being re-initialized after
-     *                           previously being shut down then this Bundle contains the data it most
-     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
-     *
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
