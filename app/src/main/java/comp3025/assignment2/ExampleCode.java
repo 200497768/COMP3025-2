39f8e68a542b136791f4fa5903d611808553041a
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-09 18:55:50 -0400

    I added the example code from the week 8 class, including an in-text citation.

---
+++ app/src/main/java/comp3025/assignment2/ExampleCode.java
@@ -0,0 +1,53 @@
+package comp3025.assignment2;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * This class includes the example code from the week 8 class (A. Perdikoulias, personal communication, March 6, 2026).
+ * We've change code slightly.
+ *
+ * @author Yatri Devangbhai Padhiyar
+ * @author Anastasios Perdikoulias
+ * @author Hao Tian
+ */
+public class ExampleCode {
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    public static void get(String url, Callback callback) {
+        Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(callback);
+    }
+
+    public void retrieve() {
+        String urlString = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR";
+
+        this.get(urlString, new Callback() {
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Objects.requireNonNull(response.body());
+
+                String responseData = response.body().string();
+                Log.i("tag", responseData);
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+        });
+    }
+}
