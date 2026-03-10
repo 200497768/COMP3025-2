93dd29a8b188f0ef68590a86156d81eed8cbc7be
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-10 15:55:02 -0400

    we added viewbinding class in ChooseCityFragment.java and we also started to change screen

--- app/src/main/java/comp3025/assignment2/views/ChooseCityFragment.java
+++ app/src/main/java/comp3025/assignment2/views/ChooseCityFragment.java
@@ -5,9 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import comp3025.assignment2.R;
+import comp3025.assignment2.databinding.FragmentChooseCityBinding;
 
 /**
  * This fragment allows a city to be chosen.
@@ -15,11 +18,28 @@ import comp3025.assignment2.R;
  * @author Hao Tian
  */
 public class ChooseCityFragment extends Fragment {
-
+    private FragmentChooseCityBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_choose_city, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Prepare the view binding class.
+        this.binding = FragmentChooseCityBinding.bind(view);
+
+        ChooseCityFragment chooseCityFragment = this;
+        this.binding.changeCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
     }
 }
