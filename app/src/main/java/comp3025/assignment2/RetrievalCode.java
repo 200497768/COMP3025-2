6acf9ea924aefe54ede9450adfcdb79fe8ce45a8
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-10 17:11:09 -0400

    I added RetrievalCode.

---
+++ app/src/main/java/comp3025/assignment2/RetrievalCode.java
@@ -0,0 +1,12 @@
+package comp3025.assignment2;
+
+/**
+ * This class is code that's used to retrieve weather information.
+ * In order to write this code, we've started with the example code from the week 8 class (A. Perdikoulias, personal communication, March 6, 2026).
+ *
+ * @author Yatri Devangbhai Padhiyar
+ * @author Anastasios Perdikoulias
+ * @author Hao Tian
+ */
+public class RetrievalCode {
+}
