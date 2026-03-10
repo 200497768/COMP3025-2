d7d576f64d9aba87e1770dfe643f2d17c6182906
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-10 16:29:48 -0400

    so i set fragment manager to show orange screen

--- app/src/main/java/comp3025/assignment2/views/MainActivity.java
+++ app/src/main/java/comp3025/assignment2/views/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import comp3025.assignment2.ExampleCode;
@@ -84,7 +87,13 @@ public class MainActivity extends AppCompatActivity {
         this.binding.changeCityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+                Fragment firstFragment = new ShowWeatherFragment();
+
+                fragmentTransaction.replace(R.id.fragment_container, firstFragment);
+                fragmentTransaction.commit();
 
             }
         });
