9d8bee02137ef874e257a8dafd4d5315f496090e
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-12 16:55:33 -0400

    We added the code again that changes every field to match the model. This allows us to determine what TextView fields we still need to add.

--- app/src/main/java/comp3025/assignment2/views/ShowWeatherFragment.java
+++ app/src/main/java/comp3025/assignment2/views/ShowWeatherFragment.java
@@ -40,7 +40,17 @@ public class ShowWeatherFragment extends Fragment {
         Log.i("200497768", "ShowWeatherFragment has received a model with " + this.weatherInformation.getCityName() + " as the city name.");
 
         //Show every field from the model.
-
+        this.binding.cityNameTextView.setText(this.weatherInformation.getCityName());
+        this.binding.countryNameTextView.setText(this.weatherInformation.getCountryName());
+        this.binding.currentTemperatureCTextView.setText(this.weatherInformation.getCurrentTemperatureC());
+        this.binding.currentTemperatureFTextView.setText(this.weatherInformation.getCurrentTemperatureF());
+        this.binding.conditionTextTextView.setText(this.weatherInformation.getWeatherConditionText());
+        this.binding.feelsLikeCTextView.setText(this.weatherInformation.getFeelsLikeC());
+        this.binding.feelsLikeFTextView.setText(this.weatherInformation.getFeelsLikeF());
+        this.binding.humidityTextView.setText(this.weatherInformation.getHumidityPercentage());
+        this.binding.windSpeedTextView.setText(this.weatherInformation.getWindSpeed());
+        this.binding.windDirectionTextTextView.setText(this.weatherInformation.getWindDirectionText());
+        this.binding.windDirectionAngleTextView.setText(this.weatherInformation.getWindDirectionAngle());
 
     }
 
