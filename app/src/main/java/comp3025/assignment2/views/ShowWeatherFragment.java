e1b6706ea29a52890aa7b253e2c204bfff946012
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-12 12:51:36 -0400

    I finished adding the code for ShowWeatherFragment that shows every field from the model.

--- app/src/main/java/comp3025/assignment2/views/ShowWeatherFragment.java
+++ app/src/main/java/comp3025/assignment2/views/ShowWeatherFragment.java
@@ -39,6 +39,14 @@ public class ShowWeatherFragment extends Fragment {
         this.binding.cityNameTextView.setText(this.weatherInformation.getCityName());
         this.binding.countryNameTextView.setText(this.weatherInformation.getCountryName());
         this.binding.currentTemperatureCTextView.setText(this.weatherInformation.getCurrentTemperatureC());
+        this.binding.currentTemperatureFTextView.setText(this.weatherInformation.getCurrentTemperatureF());
+        this.binding.conditionTextTextView.setText(this.weatherInformation.getWeatherConditionText());
+        this.binding.feelsLikeCTextView.setText(this.weatherInformation.getFeelsLikeC());
+        this.binding.feelsLikeFTextViwe.setText(this.weatherInformation.getFeelsLikeF());
+        this.binding.humidityTextView.setText(this.weatherInformation.getHumidityPercentage());
+        this.binding.windSpeedTextView.setText(this.weatherInformation.getWindSpeed());
+        this.binding.windDirectionTextTextView.setText(this.weatherInformation.getWindDirectionText());
+        this.binding.windDirectionAngle.setText(this.weatherInformation.getWindDirectionAngle());
 
     }
 
