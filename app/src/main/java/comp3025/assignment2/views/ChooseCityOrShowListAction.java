da9934bf14315587c82e7d905efc741369e47498
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-12 12:13:06 -0400

    I started writing ChooseCityOrShowListAction.

---
+++ app/src/main/java/comp3025/assignment2/views/ChooseCityOrShowListAction.java
@@ -0,0 +1,24 @@
+package comp3025.assignment2.views;
+
+/**
+ * This code determines an action that's available.
+ * The action can be to show the chosen city, or to show the list in order to choose another city.
+ * The action starts as to show the chosen city.
+ * After the action has been chosen, it changes to the other action.
+ *
+ * @author Yatri Devangbhai Padhiyar
+ * @author Hao Tian
+ */
+public class ChooseCityOrShowListAction {
+    /**
+     * This field determines whether the action is to show the chosen city, or to show the list.
+     */
+    private boolean showChooseCity;
+
+    /**
+     * This method produces different text, depending on the action.
+     */
+    public String getText() {
+        return "Method isn't written at this time.";
+    }
+}
