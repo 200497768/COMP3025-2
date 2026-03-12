a7102f6d35a9e2df4fc7e00b1175b65f3ae8979d
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-12 12:14:18 -0400

    I added a method to ChooseCityOrShowListAction.

--- app/src/main/java/comp3025/assignment2/views/ChooseCityOrShowListAction.java
+++ app/src/main/java/comp3025/assignment2/views/ChooseCityOrShowListAction.java
@@ -5,7 +5,6 @@ package comp3025.assignment2.views;
  * The action can be to show the chosen city, or to show the list in order to choose another city.
  * The action starts as to show the chosen city.
  * After the action has been chosen, it changes to the other action.
- *
  * @author Yatri Devangbhai Padhiyar
  * @author Hao Tian
  */
@@ -20,5 +19,13 @@ public class ChooseCityOrShowListAction {
      */
     public String getText() {
         return "Method isn't written at this time.";
+    }
+
+    /**
+     * This method changes the action to the other action.
+     * This method must be used when the action has happened.
+     */
+    public void change() {
+
     }
 }
