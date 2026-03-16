a2c264723f2b366914769dc5ab4766208151ee14
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-16 14:00:24 -0400

    I added the CityOptions model.

---
+++ app/src/main/java/comp3025/assignment2/models/CityOptions.java
@@ -0,0 +1,18 @@
+package comp3025.assignment2.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is a model that includes multiple city option models.
+ *
+ * @author Yatri Devangbhai Padhiyar
+ * @author Hao Tian
+ */
+public class CityOptions {
+    private List<City> cityOptions = new ArrayList<>();
+
+    public List<City> getCityOptions() {
+        return cityOptions;
+    }
+}
