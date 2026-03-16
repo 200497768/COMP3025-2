0e68b53a23637d0fa257f17b23089532f914b5ac
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-16 14:38:49 -0400

    I added the name of the city as the first field to CreatedViewHolder and CreatedAdapter.

--- app/src/main/java/comp3025/assignment2/views/choose/CreatedAdapter.java
+++ app/src/main/java/comp3025/assignment2/views/choose/CreatedAdapter.java
@@ -3,6 +3,7 @@ package comp3025.assignment2.views.choose;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -40,6 +41,7 @@ public class CreatedAdapter extends RecyclerView.Adapter<CreatedViewHolder> {
     }
 
     /**
+     * This method changes views to match the fields from the city option model.
      * In order to write this code, we've started with the example code from the week 9 class (A. Perdikoulias, personal communication, March 13, 2026).
      */
     @Override
@@ -49,7 +51,11 @@ public class CreatedAdapter extends RecyclerView.Adapter<CreatedViewHolder> {
         //Retrieve a single city option model using this number.
         City city = this.cityOptions.get(position);
 
-        //Change information to match the city option model.
+        //The remaining code for this method changes views to match the city option model.
+
+        //Change the name of the city.
+        TextView cityNameTextView = holder.getCityNameTextView();
+        cityNameTextView.setText(city.getCity());
     }
 
     /**
--- app/src/main/java/comp3025/assignment2/views/choose/CreatedViewHolder.java
+++ app/src/main/java/comp3025/assignment2/views/choose/CreatedViewHolder.java
@@ -2,9 +2,12 @@ package comp3025.assignment2.views.choose;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import comp3025.assignment2.R;
 
 /**
  * This class is needed for RecyclerView.
@@ -16,8 +19,15 @@ import androidx.recyclerview.widget.RecyclerView;
  */
 public class CreatedViewHolder extends RecyclerView.ViewHolder {
 
+    private TextView cityNameTextView;
+
     public CreatedViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        //Retrieve each view that will be needed from itemView, so that it becomes a field.
+
+        //Retrieve the TextView item that will be used to show the name of the city.
+        this.cityNameTextView = itemView.findViewById(R.id.cityNameTextView);
 
         this.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +35,9 @@ public class CreatedViewHolder extends RecyclerView.ViewHolder {
                 Log.d("200497768", "CreatedViewHolder");
             }
         });
+    }
+
+    public TextView getCityNameTextView() {
+        return cityNameTextView;
     }
 }
