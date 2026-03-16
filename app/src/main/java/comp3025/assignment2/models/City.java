b53355fd1d01ce25d2e546dfb497407255501113
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-16 11:37:49 -0400

    I added methods to the city model to allow changing and retriving the fields.

--- app/src/main/java/comp3025/assignment2/models/City.java
+++ app/src/main/java/comp3025/assignment2/models/City.java
@@ -22,7 +22,6 @@ public class City {
      */
     private String country;
 
-
     /**
      * This field needs to be provided in order to retrieve the WeatherInformation model.
      * We'll need to provide it when we retrieve the model.
@@ -36,4 +35,44 @@ public class City {
      * Since we only need to provide it, we don't need to understand what this field means.
      */
     private double lon;
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
