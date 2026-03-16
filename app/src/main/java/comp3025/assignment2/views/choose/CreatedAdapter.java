920e2dcb0e2bae130edf756ac2b53ca3764da68f
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-16 13:02:53 -0400

    I added some of the code for the item that will show a city option model.

--- app/src/main/java/comp3025/assignment2/views/choose/CreatedAdapter.java
+++ app/src/main/java/comp3025/assignment2/views/choose/CreatedAdapter.java
@@ -15,7 +15,6 @@ import comp3025.assignment2.models.City;
 /**
  * This class is needed for RecyclerView.
  * In order to write this code, we've started with the example code from the week 9 class (A. Perdikoulias, personal communication, March 13, 2026).
- *
  * @author Yatri Devangbhai Padhiyar
  * @author Anastasios Perdikoulias
  * @author Hao Tian
