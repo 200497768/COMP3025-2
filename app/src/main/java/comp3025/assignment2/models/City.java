6d549574424f6288235fe7ae060ab6f6ff5df8fe
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-16 11:31:46 -0400

    I added the city model. This model is a retrieved city option.

---
+++ app/src/main/java/comp3025/assignment2/models/City.java
@@ -0,0 +1,22 @@
+package comp3025.assignment2.models;
+
+public class City {
+    /**
+     * This field is the name of the city that's available.
+     */
+    private String name;
+
+    /**
+     * This field needs to be provided in order to retrieve the WeatherInformation model.
+     * We'll need to provide it when we retrieve the model.
+     * Since we only need to provide it, we don't need to understand what this field means.
+     */
+    private double lat;
+
+    /**
+     * This field needs to be provided in order to retrieve the WeatherInformation model.
+     * We'll need to provide it when we retrieve the model.
+     * Since we only need to provide it, we don't need to understand what this field means.
+     */
+    private double lon;
+}
