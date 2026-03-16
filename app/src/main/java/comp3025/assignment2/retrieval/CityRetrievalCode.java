e2bfa3a3acb7ee79f103a630f678de4c51ad11f4
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-16 11:13:53 -0400

    I'm writing the city retrieval code. I've started with the retrieve method from the existing retrieval code, and I'm planning to create another model for the retrieved city.

--- app/src/main/java/comp3025/assignment2/retrieval/CityRetrievalCode.java
+++ app/src/main/java/comp3025/assignment2/retrieval/CityRetrievalCode.java
@@ -1,5 +1,61 @@
 package comp3025.assignment2.retrieval;
 
-public class CityRetrievalCode {
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class CityRetrievalCode {
+    public void retrieve() {
+        String city = "Barrie";
+        String urlString = "http://api.weatherapi.com/v1/search.json?key=b47d3ee63f574764af5163148261303&q=" + city;
+
+        Request request = new Request.Builder().url(urlString).build();
+
+        OkHttpClient client = new OkHttpClient();
+
+        CityRetrievalCode retrievalCode = this;
+        client.newCall(request).enqueue(new Callback() {
+
+            /**
+             * This method creates the WeatherInformation model.
+             */
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Objects.requireNonNull(response.body());
+
+                String responseData = response.body().string();
+                Log.i("200594802 and 200497768", responseData);
+
+                //Produce the WeatherInformation model by using information from responseData.
+
+
+                //Change the field.
+
+
+                //The retrieved method needs to happen after the model has been created.
+                retrievalCode.retrieved();
+            }
+
+            /**
+             * This method doesn't create the WeatherInformation model.
+             */
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.i("200594802 and 200497768", "The WeatherInformation model wasn't retrieved.");
+                Log.i("200594802 and 200497768", "" + e);
+            }
+    });
+}
+    public void retrieved(){
+
+    }
 }
