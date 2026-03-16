5eeaff5aaac3af2295324140b9a1c41b022147ce
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-16 13:51:51 -0400

    I added the city option model as a field to ChooseCityFragmentViewModel. In addition, I fixed a mistake with the ViewModel.

--- app/src/main/java/comp3025/assignment2/views/ChooseCityFragmentViewModel.java
+++ app/src/main/java/comp3025/assignment2/views/ChooseCityFragmentViewModel.java
@@ -1,11 +1,19 @@
 package comp3025.assignment2.views;
 
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import comp3025.assignment2.models.City;
+
 /**
  * This class is the ViewModel for ChooseCityFragment.
- *
  * @author Yatri Devangbhai Padhiyar
  * @author Hao Tian
  */
-public class ChooseCityFragmentViewModel {
+public class ChooseCityFragmentViewModel extends ViewModel {
+    private MutableLiveData<City> mutableLiveData = new MutableLiveData<>();
 
+    public MutableLiveData<City> getMutableLiveData() {
+        return mutableLiveData;
+    }
 }
