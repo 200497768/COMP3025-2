a8310ad8b279870bc1a32f7cbb3f6f4624d2be1a
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-16 16:24:06 -0400

    We added text for the cityOptionChosen method to explain what needs to happen when the code receives the city option model from ChooseCityFragment.

--- app/src/main/java/comp3025/assignment2/views/MainActivity.java
+++ app/src/main/java/comp3025/assignment2/views/MainActivity.java
@@ -154,6 +154,12 @@ public class MainActivity extends AppCompatActivity {
         //This is the action that needs to happen when a city has been chosen during ChooseCityFragment.
         CityOptionChosenAction cityOptionChosenAction = new CityOptionChosenAction() {
 
+            /**
+             * This method happens when ChooseCityFragment has produced a city option model.
+             * In other words, this method happens when a city has been chosen.
+             * This method is responsible for providing the chosen city to the ViewModel for MainActivity.
+             * The ViewModel will retrieve the WeatherInformation model, and change the fragment area.
+             */
             @Override
             public void cityOptionChosen(City city) {
 
