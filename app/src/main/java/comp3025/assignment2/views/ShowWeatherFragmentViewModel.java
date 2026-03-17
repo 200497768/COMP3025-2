c496fab20fe8edc781b34f5917cb25bb7b2d4000
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-17 11:08:36 -0400

    I created ShowWeatherFragmentViewModel. I still need to change the fields to the ViewModel.

---
+++ app/src/main/java/comp3025/assignment2/views/ShowWeatherFragmentViewModel.java
@@ -0,0 +1,10 @@
+package comp3025.assignment2.views;
+
+/**
+ * This class is the ViewModel for ShowWeatherFragment.
+ *
+ * @author Yatri Devangbhai Padhiyar
+ * @author Hao Tian
+ */
+public class ShowWeatherFragmentViewModel {
+}
