f6f86026c40b29feb83b4561e8d4228c02ce4fde
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-31 17:00:00 -0400

    I added code for the ViewModel and view binding class to LoginRegistrationActivity.

--- app/src/main/java/comp3025/assignment2/views/LoginRegistrationActivity.java
+++ app/src/main/java/comp3025/assignment2/views/LoginRegistrationActivity.java
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import comp3025.assignment2.R;
+import comp3025.assignment2.databinding.ActivityLoginRegistrationBinding;
 
 /**
  * This activity shows LoginFragment and RegistrationFragment.
@@ -20,11 +21,34 @@ import comp3025.assignment2.R;
  */
 public class LoginRegistrationActivity extends AppCompatActivity {
 
+    /**
+     * This is the view binding class for this activity.
+     */
+    private ActivityLoginRegistrationBinding binding;
+
+    /**
+     * This is the ViewModel for this activity.
+     */
+    private LoginRegistrationActivityViewModel viewModel;
+
+    /**
+     * The onCreate method shows the fragment area.
+     * This method might happen repeatedly.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_registration);
+
+        //Prepare the view binding class.
+        this.binding = ActivityLoginRegistrationBinding.inflate(getLayoutInflater());
+
+        //Change the view for this activity.
+        //We'll access the view through the view binding class, instead of the R.layout.activity_main.
+        setContentView(this.binding.getRoot());
+
+        //Order is important, and the setOnApplyWindowInsetsListener method must be after the setContentView method.
+        //If the setContentView method hasn't completed, the view will be null.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/comp3025/assignment2/views/LoginRegistrationActivityViewModel.java
@@ -0,0 +1,14 @@
+package comp3025.assignment2.views;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * This is the ViewModel for LoginRegistrationActivity.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
+public class LoginRegistrationActivityViewModel extends ViewModel {
+}
