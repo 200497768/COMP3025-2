17263869b42855a816cf9e0a4e2151c13d5323c3
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-31 17:33:17 -0400

    I added models for LoginInformation and RegistrationInformation.

---
+++ app/src/main/java/comp3025/assignment2/models/LoginInformation.java
@@ -0,0 +1,14 @@
+package comp3025.assignment2.models;
+
+/**
+ * This model is created during LoginFragment.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
+public class LoginInformation {
+    private String studentNumber;
+    private String password;
+}
