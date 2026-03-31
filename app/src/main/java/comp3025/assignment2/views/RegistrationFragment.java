1add7ce1110a19952a8177e4dfcb88b7ab3fb38f
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-31 16:17:51 -0400

    I created LoginFragment and RegistrationFragment.

---
+++ app/src/main/java/comp3025/assignment2/views/RegistrationFragment.java
@@ -0,0 +1,30 @@
+package comp3025.assignment2.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import comp3025.assignment2.R;
+
+/**
+ * This fragment includes views for registration.
+ * Registration refers to adding a person's login information to the system for the first time.
+ * This can be thought of as registering courses.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
+public class RegistrationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+}
