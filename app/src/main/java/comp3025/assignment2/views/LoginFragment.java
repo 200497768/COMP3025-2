25bc0d5136805c1d945d60e80dd1f19e72c0bee6
200497768 <169184214+200497768@users.noreply.github.com>
2026-03-31 16:41:26 -0400

    I added the view binding class as a field to LoginFragment.

--- app/src/main/java/comp3025/assignment2/views/LoginFragment.java
+++ app/src/main/java/comp3025/assignment2/views/LoginFragment.java
@@ -5,9 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import comp3025.assignment2.R;
+import comp3025.assignment2.databinding.FragmentLoginBinding;
 
 /**
  * This fragment includes views for writing login information.
@@ -19,10 +22,28 @@ import comp3025.assignment2.R;
  */
 public class LoginFragment extends Fragment {
 
+    /**
+     * This field is the view binding class.
+     */
+    private FragmentLoginBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+
+    /**
+     * This method prepares the view binding class, and shows every field from the model.
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        //The view binding class can only be accessed after it has been created, like during this method.
+        //We're changing text during this method because the view binding class can't be accessed before this method.
+
+        //Prepare the view binding class.
+        this.binding = FragmentLoginBinding.bind(view);
     }
 }
