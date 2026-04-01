f51c79824d0638970272beaa203f6afb07e26dfd
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-01 13:42:17 -0400

    WRS-29 Fix mistake with LoginFragment by changing completedAction field to the ViewModel (#18)
    * I added outputTextView to LoginFragment. This is the view that will show whether login information was correctly written.
    
    * I added code so that the fragment won't show outputTextView when the fragment has been created, and before login information has been written.
    
    * I fixed a problem that caused the completed action field to not be available after LoginFragment needed to be created again.

--- app/src/main/java/comp3025/assignment2/viewmodels/LoginFragmentViewModel.java
+++ app/src/main/java/comp3025/assignment2/viewmodels/LoginFragmentViewModel.java
@@ -1,6 +1,9 @@
 package comp3025.assignment2.viewmodels;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import comp3025.assignment2.views.CompletedAction;
 
 /**
  * This class is the ViewModel for LoginFragment.
@@ -11,4 +14,17 @@ import androidx.lifecycle.ViewModel;
  * @author Hao Tian
  */
 public class LoginFragmentViewModel extends ViewModel {
+
+    /**
+     * This field is the action that needs to happen after the login task has completed.
+     */
+    private MutableLiveData<CompletedAction> completedAction = new MutableLiveData<>();
+
+    public MutableLiveData<CompletedAction> getCompletedAction() {
+        return completedAction;
+    }
+
+    public void setCompletedAction(CompletedAction completedAction) {
+        this.completedAction.postValue(completedAction);
+    }
 }
