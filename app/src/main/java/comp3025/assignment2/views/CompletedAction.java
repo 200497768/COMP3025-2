4b95c171757758a2e367ef3a11707f582864df93
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-06 14:48:14 -0400

    I added text explaining CityOptionChosenAction.

--- app/src/main/java/comp3025/assignment2/views/CompletedAction.java
+++ app/src/main/java/comp3025/assignment2/views/CompletedAction.java
@@ -2,7 +2,6 @@ package comp3025.assignment2.views;
 
 /**
  * This class is an action that needs to happen after a fragment has completed a task.
- *
  * @author Harshit Gambhir
  * @author Yatri Devangbhai Padhiyar
  * @author Dawa Angchuk Sherpa
