4a813f39258e6ab7138fef760536ff2a6f51d5da
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-06 15:32:19 -0400

    I added text explaining CityOptions. I still need to change the code for the addCityOption method so that it doesn't add the city option model if it's not provided.

--- app/src/main/java/comp3025/assignment2/models/CityOptions.java
+++ app/src/main/java/comp3025/assignment2/models/CityOptions.java
@@ -11,12 +11,26 @@ import java.util.List;
  * @author Hao Tian
  */
 public class CityOptions {
+
+    /**
+     * This field is the city option models that have been added.
+     * CityOptions starts with no models.
+     * A city option model can be added using the addCityOption method.
+     */
     private List<CityOption> cityOptions = new ArrayList<>();
 
+    /**
+     * This method returns the city option models that have been added.
+     */
     public List<CityOption> getCityOptions() {
         return cityOptions;
     }
 
+    /**
+     * This method adds a city option model.
+     * This method only adds a model if a model has been provided.
+     * If a model isn't provided, the method won't add it.
+     */
     public void addCityOption(CityOption cityOption) {
         this.cityOptions.add(cityOption);
     }
