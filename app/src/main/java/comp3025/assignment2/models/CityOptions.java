8c87ad35acf216843fe1221fae28a39259222637
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-06 15:35:33 -0400

    WRS-31 Change CityOptions so that the model won’t be added if it wasn’t provided (#28)
    * I added text explaining CityOptions. I still need to change the code for the addCityOption method so that it doesn't add the city option model if it's not provided.
    
    * I changed the addCityOption method so that the city option model will only be added if it's provided to the method.

--- app/src/main/java/comp3025/assignment2/models/CityOptions.java
+++ app/src/main/java/comp3025/assignment2/models/CityOptions.java
@@ -1,5 +1,7 @@
 package comp3025.assignment2.models;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,13 +13,36 @@ import java.util.List;
  * @author Hao Tian
  */
 public class CityOptions {
+
+    /**
+     * This field is the city option models that have been added.
+     * CityOptions starts with no models.
+     * A city option model can be added using the addCityOption method.
+     */
     private List<CityOption> cityOptions = new ArrayList<>();
 
+    /**
+     * This method returns the city option models that have been added.
+     */
     public List<CityOption> getCityOptions() {
         return cityOptions;
     }
 
+    /**
+     * This method adds a city option model.
+     * This method only adds a model if a model has been provided.
+     * If a model isn't provided, the method won't add it.
+     */
     public void addCityOption(CityOption cityOption) {
-        this.cityOptions.add(cityOption);
+        //Only add the city option model if it was provided to this method.
+        if (cityOption == null) {
+            //A city option model wasn't provided to this method.
+            Log.i("200594802 and 200497768", "A city option model wasn't added because the model wasn't provided to CityOptions.");
+        } else {
+            //Add the city option model.
+            this.cityOptions.add(cityOption);
+        }
+
+
     }
 }
