edccb5c00b0caf1484bbe94156835e4ecc411764
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-10 16:59:46 -0400

    I changed the example sound so that it doesn't mention whether it was recorded by a single student or the group.

--- app/src/main/java/comp3025/assignment2/sounds/ExampleSound.java
+++ app/src/main/java/comp3025/assignment2/sounds/ExampleSound.java
@@ -4,7 +4,7 @@ import comp3025.assignment2.R;
 
 /**
  * This is the example sound.
- * The example sound was recorded by a student in the group during 10-APR-2026 at C building.
+ * The example sound was recorded by the group during 10-APR-2026 at C building.
  * This sound doesn't include any words, so it's not the same as most of the sounds in this assignment.
  * @author Harshit Gambhir
  * @author Yatri Devangbhai Padhiyar
