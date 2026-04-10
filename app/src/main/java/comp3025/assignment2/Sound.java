ad05d7e8fda2a8cd4af94683a1c7e84e9dc8ff7f
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-10 13:58:32 -0400

    I changed the code for sound so that the number can be provided when it's created.

--- app/src/main/java/comp3025/assignment2/Sound.java
+++ app/src/main/java/comp3025/assignment2/Sound.java
@@ -1,8 +1,33 @@
 package comp3025.assignment2;
 
+/**
+ * This class is a sound that the assignment is capable of.
+ * Every sound in this assignment has been recorded by students in our group.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
 public class Sound {
-    public int getNumber(){
-        int number= R.raw.recording;
+
+    /**
+     * This field is the number that allows this sound to be accessed.
+     * The sound must be added to the code before it can be accessed.
+     * Sounds need to be added to raw.
+     */
+    private int number;
+
+    public Sound(int number) {
+        this.number = number;
+    }
+
+    /**
+     * This method returns the number that's used when accessing this number.
+     * This is the R.raw.sound number.
+     */
+    public int getNumber() {
         return number;
     }
+
 }
