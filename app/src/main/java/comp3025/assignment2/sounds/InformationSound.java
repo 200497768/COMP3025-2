a4041c5b9ad596930d6a02bad99c916d4d6f2a1f
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-10 17:06:23 -0400

    I added the information sound to the code.

---
+++ app/src/main/java/comp3025/assignment2/sounds/InformationSound.java
@@ -0,0 +1,17 @@
+package comp3025.assignment2.sounds;
+
+import comp3025.assignment2.R;
+
+/**
+ * The information sound happens when writing login information.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
+public class InformationSound extends Sound {
+    public InformationSound() {
+        super(R.raw.information);
+    }
+}
