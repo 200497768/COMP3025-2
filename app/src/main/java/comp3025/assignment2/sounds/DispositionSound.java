c98f248087bc00b96cf6cc0c0ecd57867214e7f3
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-10 17:04:09 -0400

    I added the disposition sound to the code by adding the rest of the code that needed to be added.

--- app/src/main/java/comp3025/assignment2/sounds/DispositionSound.java
+++ app/src/main/java/comp3025/assignment2/sounds/DispositionSound.java
@@ -1,4 +1,17 @@
 package comp3025.assignment2.sounds;
 
-public class DispositionSound {
+import comp3025.assignment2.R;
+
+/**
+ * The disposition sound happens when showing DispositionActivity.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
+public class DispositionSound extends Sound {
+    public DispositionSound() {
+        super(R.raw.disposition);
+    }
 }
