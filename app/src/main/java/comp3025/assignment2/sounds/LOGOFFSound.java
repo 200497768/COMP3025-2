e2059ba77f86f19dd8e60ae7fb4c7b3d6390cf6d
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-10 17:02:39 -0400

    I added the LOGOFF sound to the code.

---
+++ app/src/main/java/comp3025/assignment2/sounds/LOGOFFSound.java
@@ -0,0 +1,17 @@
+package comp3025.assignment2.sounds;
+
+import comp3025.assignment2.R;
+
+/**
+ * This sound happens when showing LOGOFF.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
+public class LOGOFFSound extends Sound {
+    public LOGOFFSound() {
+        super(R.raw.logoff);
+    }
+}
