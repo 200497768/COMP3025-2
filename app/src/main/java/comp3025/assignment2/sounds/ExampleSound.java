8084c0774d2487f913c945d6122d2ce1eae9607b
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-10 14:09:45 -0400

    I fixed a mistake with the example sound so that a number is no longer needed when creating it.

--- app/src/main/java/comp3025/assignment2/sounds/ExampleSound.java
+++ app/src/main/java/comp3025/assignment2/sounds/ExampleSound.java
@@ -6,14 +6,13 @@ import comp3025.assignment2.R;
  * This is the example sound.
  * The example sound was recorded by a student in the group during 10-APR-2026 at C building.
  * This sound doesn't include any words, so it's not the same as most of the sounds in this assignment.
- *
  * @author Harshit Gambhir
  * @author Yatri Devangbhai Padhiyar
  * @author Dawa Angchuk Sherpa
  * @author Hao Tian
  */
 public class ExampleSound extends Sound {
-    public ExampleSound(int number) {
+    public ExampleSound() {
         super(R.raw.example);
     }
 }
