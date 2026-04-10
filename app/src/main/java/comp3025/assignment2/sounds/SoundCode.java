bc8cc2a3fc31127a90e3f913fe92f517b8e7f2fb
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-10 14:01:51 -0400

    I added code to SoundCode to allow changing the context field when it's being created.

--- app/src/main/java/comp3025/assignment2/sounds/SoundCode.java
+++ app/src/main/java/comp3025/assignment2/sounds/SoundCode.java
@@ -17,6 +17,10 @@ public class SoundCode {
      */
     private Context context;
 
+    public SoundCode(Context context) {
+        this.context = context;
+    }
+
     /**
      * This method starts a sound.
      */
