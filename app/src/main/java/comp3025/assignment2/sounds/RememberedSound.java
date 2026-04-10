56d6e1f14ccb7d1a726a20644be914982e6cd8a1
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-10 17:39:28 -0400

    I added the code for the remembered sound.

---
+++ app/src/main/java/comp3025/assignment2/sounds/RememberedSound.java
@@ -0,0 +1,17 @@
+package comp3025.assignment2.sounds;
+
+import comp3025.assignment2.R;
+
+/**
+ * The remembered sound happens when adding a city to the remembered city list.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
+public class RememberedSound extends Sound {
+    public RememberedSound() {
+        super(R.raw.remembered);
+    }
+}
