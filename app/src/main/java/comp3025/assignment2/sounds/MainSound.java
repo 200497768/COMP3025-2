fbd5b6675724469a51c2ad52bf28f34af2f3d1b1
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-13 17:36:40 -0400

    I added text for MainSound.

--- app/src/main/java/comp3025/assignment2/sounds/MainSound.java
+++ app/src/main/java/comp3025/assignment2/sounds/MainSound.java
@@ -4,7 +4,11 @@ import comp3025.assignment2.R;
 
 /**
  * The main sound happens when showing MainActivity.
- *
+ * This is the main screen of the weather retrieval system.
+ * Write the name of a city to retrieve weather information for that city.
+ * Select the desired city option that appears to retrieve weather information for that city.
+ * If the desired city option doesn’t appear after writing the name of the city, select Retrieve.
+ * Select LOGOFF if you have finished using the system.
  * @author Harshit Gambhir
  * @author Yatri Devangbhai Padhiyar
  * @author Dawa Angchuk Sherpa
