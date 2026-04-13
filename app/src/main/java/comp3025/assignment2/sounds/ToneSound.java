6ffc7f527d7d2fd100e36c55922d73dfcb0fc2b0
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-13 14:25:07 -0400

    I added the code for the tone sound.

---
+++ app/src/main/java/comp3025/assignment2/sounds/ToneSound.java
@@ -0,0 +1,17 @@
+package comp3025.assignment2.sounds;
+
+import comp3025.assignment2.R;
+
+/**
+ * The tone sound happens during the assignment.
+ *
+ * @author Harshit Gambhir
+ * @author Yatri Devangbhai Padhiyar
+ * @author Dawa Angchuk Sherpa
+ * @author Hao Tian
+ */
+public class ToneSound extends Sound {
+    public ToneSound(int number) {
+        super(R.raw.tone1);
+    }
+}
