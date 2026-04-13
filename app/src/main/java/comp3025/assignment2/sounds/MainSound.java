299e7d0fdc051145f6dc20095e677fd6cd448976
200497768 <169184214+200497768@users.noreply.github.com>
2026-04-13 17:54:15 -0400

    WRS-42 I recorded the main sound again in order to remove repeated words. (#48)
    * I added text for MainSound.
    
    * I changed text for MainSound to remove some repeated words.
    
    * I recorded the main sound again.

--- app/src/main/java/comp3025/assignment2/sounds/MainSound.java
+++ app/src/main/java/comp3025/assignment2/sounds/MainSound.java
@@ -4,7 +4,10 @@ import comp3025.assignment2.R;
 
 /**
  * The main sound happens when showing MainActivity.
- *
+ * This is the main screen of the weather retrieval system.
+ * Write the name of a city, and select the desired city option that appears to retrieve weather information for that city.
+ * If the desired city option doesn’t appear after writing the name of the city, select Retrieve.
+ * Select LOGOFF when you have finished using the system.
  * @author Harshit Gambhir
  * @author Yatri Devangbhai Padhiyar
  * @author Dawa Angchuk Sherpa
